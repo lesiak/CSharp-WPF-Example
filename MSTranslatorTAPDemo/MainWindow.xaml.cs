58b93a99ffc1a8ebae585bc8b1152ba06b26721e
lesiak <lesiak@outlook.com>
2017-06-06 12:01:48 +0200

    Extracted class

--- MSTranslatorTAPDemo/MainWindow.xaml.cs
+++ MSTranslatorTAPDemo/MainWindow.xaml.cs
@@ -36,7 +36,7 @@ namespace MSTranslatorTAPDemo
             tokenProvider = new AzureAuthToken(TEXT_TRANSLATION_API_SUBSCRIPTION_KEY);
             var languageCodes = GetLanguageCodesForTranslate().ToArray();
             GetLanguageNamesMethod(tokenProvider.GetAccessToken(), languageCodes);
-            GetLanguagesForSpeakMethod(tokenProvider
+            speakLanguages = TranslateApi.GetLanguagesForSpeakMethod(tokenProvider
                 .GetAccessToken()); //List of languages that have a synthetic voice for text to speech
             enumLanguages(); //Create the drop down list of langauges
         }
@@ -211,39 +211,6 @@ namespace MSTranslatorTAPDemo
             }
         }
 
-        private void GetLanguagesForSpeakMethod(string authToken)
-        {
-            const string uri = "http://api.microsofttranslator.com/v2/Http.svc/GetLanguagesForSpeak";
-            speakLanguages = GetAndDeserialize(uri, authToken, DeserializeFromStream<List<string>>);
-        }
-
-        private static T GetAndDeserialize<T>(string uri, string authToken, Func<Stream, T> deserializeFunc)
-        {
-            var httpWebRequest = WebRequest.Create(uri);
-            httpWebRequest.Headers.Add("Authorization", authToken);
-            WebResponse response = null;
-            try
-            {
-                response = httpWebRequest.GetResponse();
-                using (var stream = response.GetResponseStream())
-                {
-                    return deserializeFunc(stream);
-                }
-            }
-            finally
-            {
-                if (response != null)
-                {
-                    response.Close();
-                }
-            }
-        }
-
-        private static T DeserializeFromStream<T>(Stream stream)
-        {
-            var serializer = new DataContractSerializer(typeof(T));
-            var retList = (T)serializer.ReadObject(stream);
-            return retList;
-        }
+       
     }
 }
---
+++ MSTranslatorTAPDemo/TranslateApi.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Runtime.Serialization;
+
+namespace MSTranslatorTAPDemo
+{
+    public class TranslateApi
+    {
+        /// <summary>
+        /// List of languages that have a synthetic voice for text to speech
+        /// </summary>
+        /// <param name="authToken"></param>
+        /// <returns></returns>
+        public static List<string> GetLanguagesForSpeakMethod(string authToken)
+        {
+            const string uri = "http://api.microsofttranslator.com/v2/Http.svc/GetLanguagesForSpeak";
+            return GetAndDeserialize(uri, authToken, DeserializeFromStream<List<string>>);
+        }
+
+        private static T GetAndDeserialize<T>(string uri, string authToken, Func<Stream, T> deserializeFunc)
+        {
+            var httpWebRequest = WebRequest.Create(uri);
+            httpWebRequest.Headers.Add("Authorization", authToken);
+            WebResponse response = null;
+            try
+            {
+                response = httpWebRequest.GetResponse();
+                using (var stream = response.GetResponseStream())
+                {
+                    return deserializeFunc(stream);
+                }
+            }
+            finally
+            {
+                if (response != null)
+                {
+                    response.Close();
+                }
+            }
+        }
+
+        private static T DeserializeFromStream<T>(Stream stream)
+        {
+            var serializer = new DataContractSerializer(typeof(T));
+            var retList = (T)serializer.ReadObject(stream);
+            return retList;
+        }
+    }
+}
