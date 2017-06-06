164405a318b724f1e337992a1d4520a9bfd78d27
lesiak <lesiak@outlook.com>
2017-06-06 11:48:37 +0200

    Extracted methods

--- MSTranslatorTAPDemo/MainWindow.xaml.cs
+++ MSTranslatorTAPDemo/MainWindow.xaml.cs
@@ -6,6 +6,7 @@ using System.IO;
 using System.Media;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Text;
 using Microsoft.Translator.Samples;
 
@@ -34,8 +35,10 @@ namespace MSTranslatorTAPDemo
             InitializeComponent();
             tokenProvider = new AzureAuthToken(TEXT_TRANSLATION_API_SUBSCRIPTION_KEY);
             GetLanguagesForTranslate(); //List of languages that can be translated
-            GetLanguageNamesMethod(tokenProvider.GetAccessToken(), friendlyName); //Friendly name of languages that can be translated
-            GetLanguagesForSpeakMethod(tokenProvider.GetAccessToken()); //List of languages that have a synthetic voice for text to speech
+            GetLanguageNamesMethod(tokenProvider.GetAccessToken(),
+                friendlyName); //Friendly name of languages that can be translated
+            GetLanguagesForSpeakMethod(tokenProvider
+                .GetAccessToken()); //List of languages that have a synthetic voice for text to speech
             enumLanguages(); //Create the drop down list of langauges
         }
 
@@ -55,23 +58,27 @@ namespace MSTranslatorTAPDemo
         private void translateButton_Click(object sender, EventArgs e)
         {
             string languageCode;
-            languageCodesAndTitles.TryGetValue(LanguageComboBox.Text, out languageCode); //get the language code from the dictionary based on the selection in the combobox
+            languageCodesAndTitles.TryGetValue(LanguageComboBox.Text,
+                out languageCode); //get the language code from the dictionary based on the selection in the combobox
 
-            if (languageCode == null)  //in case no language is selected.
+            if (languageCode == null) //in case no language is selected.
             {
                 languageCode = "en";
-
             }
 
             //*****BEGIN CODE TO MAKE THE CALL TO THE TRANSLATOR SERVICE TO PERFORM A TRANSLATION FROM THE USER TEXT ENTERED INCLUDES A CALL TO A SPEECH METHOD*****
 
             string txtToTranslate = textToTranslate.Text;
 
-            string uri = string.Format("http://api.microsofttranslator.com/v2/Http.svc/Translate?text=" + System.Web.HttpUtility.UrlEncode(txtToTranslate) + "&to={0}", languageCode);
-           
+            string uri =
+                string.Format(
+                    "http://api.microsofttranslator.com/v2/Http.svc/Translate?text=" +
+                    System.Web.HttpUtility.UrlEncode(txtToTranslate) + "&to={0}", languageCode);
+
             WebRequest translationWebRequest = WebRequest.Create(uri);
 
-            translationWebRequest.Headers.Add("Authorization", tokenProvider.GetAccessToken()); //header value is the "Bearer plus the token from ADM
+            translationWebRequest.Headers.Add("Authorization",
+                tokenProvider.GetAccessToken()); //header value is the "Bearer plus the token from ADM
 
             WebResponse response = null;
 
@@ -87,7 +94,7 @@ namespace MSTranslatorTAPDemo
 
             xTranslation.LoadXml(translatedStream.ReadToEnd());
 
-            translatedTextLabel.Content = "Translation -->   " + xTranslation.InnerText;
+            translatedTextLabel.Content = DateTime.Now + "Translation -->   " + xTranslation.InnerText;
 
             if (speakLanguages.Contains(languageCode) && txtToTranslate != "")
             {
@@ -100,8 +107,11 @@ namespace MSTranslatorTAPDemo
         private void SpeakMethod(string authToken, string textToVoice, String languageCode)
         {
             string translatedString = textToVoice;
-            
-            string uri = string.Format("http://api.microsofttranslator.com/v2/Http.svc/Speak?text={0}&language={1}&format=" + HttpUtility.UrlEncode("audio/wav") + "&options=MaxQuality", translatedString, languageCode);
+
+            string uri =
+                string.Format(
+                    "http://api.microsofttranslator.com/v2/Http.svc/Speak?text={0}&language={1}&format=" +
+                    HttpUtility.UrlEncode("audio/wav") + "&options=MaxQuality", translatedString, languageCode);
 
             WebRequest webRequest = WebRequest.Create(uri);
             webRequest.Headers.Add("Authorization", authToken);
@@ -120,7 +130,6 @@ namespace MSTranslatorTAPDemo
             }
             catch
             {
-
                 throw;
             }
             finally
@@ -137,7 +146,6 @@ namespace MSTranslatorTAPDemo
         //*****CODE TO GET TRANSLATABLE LANGAUGE CODES*****
         private void GetLanguagesForTranslate()
         {
-           
             string uri = "http://api.microsofttranslator.com/v2/Http.svc/GetLanguagesForTranslate";
             WebRequest WebRequest = WebRequest.Create(uri);
             WebRequest.Headers.Add("Authorization", tokenProvider.GetAccessToken());
@@ -149,11 +157,12 @@ namespace MSTranslatorTAPDemo
                 response = WebRequest.GetResponse();
                 using (Stream stream = response.GetResponseStream())
                 {
-
-                    System.Runtime.Serialization.DataContractSerializer dcs = new System.Runtime.Serialization.DataContractSerializer(typeof(List<string>));
-                    List<string> languagesForTranslate = (List<string>)dcs.ReadObject(stream);
-                    friendlyName = languagesForTranslate.ToArray(); //put the list of language codes into an array to pass to the method to get the friendly name.
-                    
+                    System.Runtime.Serialization.DataContractSerializer dcs =
+                        new System.Runtime.Serialization.DataContractSerializer(typeof(List<string>));
+                    List<string> languagesForTranslate = (List<string>) dcs.ReadObject(stream);
+                    friendlyName =
+                        languagesForTranslate
+                            .ToArray(); //put the list of language codes into an array to pass to the method to get the friendly name.
                 }
             }
             catch
@@ -176,11 +185,11 @@ namespace MSTranslatorTAPDemo
         {
             string uri = "http://api.microsofttranslator.com/v2/Http.svc/GetLanguageNames?locale=en";
             // create the request
-            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(uri);
+            HttpWebRequest request = (HttpWebRequest) WebRequest.Create(uri);
             request.Headers.Add("Authorization", tokenProvider.GetAccessToken());
             request.ContentType = "text/xml";
             request.Method = "POST";
-            System.Runtime.Serialization.DataContractSerializer dcs = new System.Runtime.Serialization.DataContractSerializer(Type.GetType("System.String[]"));
+            DataContractSerializer dcs = new DataContractSerializer(Type.GetType("System.String[]"));
             using (System.IO.Stream stream = request.GetRequestStream())
             {
                 dcs.WriteObject(stream, languageCodes);
@@ -192,14 +201,13 @@ namespace MSTranslatorTAPDemo
 
                 using (Stream stream = response.GetResponseStream())
                 {
-                    string[] languageNames = (string[])dcs.ReadObject(stream);
+                    string[] languageNames = (string[]) dcs.ReadObject(stream);
 
                     for (int i = 0; i < languageNames.Length; i++)
                     {
-
-                        languageCodesAndTitles.Add(languageNames[i], languageCodes[i]); //load the dictionary for the combo box
-
-                    }   
+                        languageCodesAndTitles.Add(languageNames[i],
+                            languageCodes[i]); //load the dictionary for the combo box
+                    }
                 }
             }
             catch
@@ -218,20 +226,21 @@ namespace MSTranslatorTAPDemo
 
         private void GetLanguagesForSpeakMethod(string authToken)
         {
+            const string uri = "http://api.microsofttranslator.com/v2/Http.svc/GetLanguagesForSpeak";
+            speakLanguages = GetAndDeserialize(uri, authToken, DeserializeFromStream<List<string>>);
+        }
 
-            string uri = "http://api.microsofttranslator.com/v2/Http.svc/GetLanguagesForSpeak";
-            HttpWebRequest httpWebRequest = (HttpWebRequest)WebRequest.Create(uri);
+        private static T GetAndDeserialize<T>(string uri, string authToken, Func<Stream, T> deserializeFunc)
+        {
+            var httpWebRequest = WebRequest.Create(uri);
             httpWebRequest.Headers.Add("Authorization", authToken);
             WebResponse response = null;
             try
             {
                 response = httpWebRequest.GetResponse();
-                using (Stream stream = response.GetResponseStream())
+                using (var stream = response.GetResponseStream())
                 {
-
-                    System.Runtime.Serialization.DataContractSerializer dcs = new System.Runtime.Serialization.DataContractSerializer(typeof(List<string>));
-                    speakLanguages = (List<string>)dcs.ReadObject(stream);
-
+                    return deserializeFunc(stream);
                 }
             }
             catch
@@ -243,9 +252,15 @@ namespace MSTranslatorTAPDemo
                 if (response != null)
                 {
                     response.Close();
-                    response = null;
                 }
             }
+        }
+
+        private static T DeserializeFromStream<T>(Stream stream)
+        {
+            var serializer = new DataContractSerializer(typeof(T));
+            var retList = (T)serializer.ReadObject(stream);
+            return retList;
         }
     }
 }
